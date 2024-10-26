197be216968e99c4606a0f2656f4376bd159bc2c
madhu2904 <madhumithavelusamy0@gmail.com>
2024-10-26 21:04:14 +0530

    imported image

--- src/json_files/data.js
+++ src/json_files/data.js
@@ -4,14 +4,14 @@ const data=[
       "id": 1,
       "title": "Landing Page",
       "description": "A responsive landing page designed to showcase a product with a clean and modern UI.",
-      "image":{landing},
+      "image":landing,
       "github":"https://github.com/VHarini3/Challenge_"
     },
     {
       "id": 2,
       "title": "Portfolio Website",
       "description": "An interactive portfolio website to display personal projects and skills with an elegant layout.",
-      "image":{landing},
+      "image":landing,
       "github":"https://github.com/VHarini3/Challenge_"
     }
   ]
